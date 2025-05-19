b474d21297e9c44ddc14df5a9656791d128a9b2b
Kodik3 <boss.barbashin10@gmail.com>
2025-05-19 15:26:09 +0500

    add formatting & collected all packages from git into one file

--- lua/plugins/fastcomment.lua
+++ lua/plugins/fastcomment.lua
@@ -26,21 +26,21 @@ return {
 
     local api = require('Comment.api')
 
-    vim.keymap.set("n", "<C-_>", function()
+    vim.keymap.set('n', '<C-_>', function()
       api.toggle.linewise.current()
-    end, { desc = "Toggle comment line (Ctrl+/)" })
+    end, { desc = 'Toggle comment line (Ctrl+/)' })
 
-    vim.keymap.set("v", "<C-_>", function()
+    vim.keymap.set('v', '<C-_>', function()
       local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
       vim.api.nvim_feedkeys(esc, 'x', false)
       api.toggle.linewise(vim.fn.visualmode())
-    end, { desc = "Toggle comment selection (Ctrl+/)" })
+    end, { desc = 'Toggle comment selection (Ctrl+/)' })
 
-    vim.keymap.set("i", "<C-_>", function()
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "x", false)
-      require("Comment.api").toggle.linewise.current()
-      vim.api.nvim_feedkeys("A", "n", false)
-    end, { desc = "Toggle comment from insert mode" })
+    vim.keymap.set('i', '<C-_>', function()
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'x', false)
+      require('Comment.api').toggle.linewise.current()
+      vim.api.nvim_feedkeys('A', 'n', false)
+    end, { desc = 'Toggle comment from insert mode' })
 
   end
 }
