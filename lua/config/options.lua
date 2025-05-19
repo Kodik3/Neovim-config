ad5a4c1a6613dc81592a95b687af5dfc117ee4f0
kodik3 <boss.barbashin10@gmail.com>
2025-05-19 17:13:01 +0500

    update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,61 +1,77 @@
+vim.o.foldenable = true     -- Enable folding by default
+vim.o.foldmethod = 'manual' -- Default fold method (change as needed)
+vim.o.foldlevel = 99        -- Open most folds by default
+vim.o.foldcolumn = '0'
+
+vim.opt.isfname:append("@-@")
+
 local options = {
-  number = true,
-  relativenumber = false,
-  signcolumn = 'yes',
-  laststatus = 3,
-  showmode = false,
-  cmdheight = 0,
-  scrolloff = 8,
-  pumheight = 10,
-  showtabline = 2,
-  mouse = 'a',
-  mousefocus = true,
-  expandtab = true,
+  nu = true,
+  relativenumber = true,
+
   tabstop = 2,
   softtabstop = 2,
   shiftwidth = 2,
+  expandtab = true,
   autoindent = true,
   smartindent = true,
+  wrap = false,
+
+  swapfile = false,
+  backup = false,
+  undofile = true,
+
+  incsearch = true,
+  inccommand = 'split',
+
+  ignorecase = true,
+  smartcase = true,
+
+  termguicolors = true,
+  background = 'dark',
+
+  scrolloff = 8,
+  signcolumn = 'yes',
+
+  backspace = { "start", "eol", "indent" },
+  splitright = true,
+  splitbelow = true,
+
+  updatetime = 50,
+  colorcolumn = '80',
+  hlsearch = true,
+
+  mouse = 'a',
+  mousefocus = true,
+
+  laststatus = 3,
+  showmode = false,
+  cmdheight = 0,
+  pumheight = 10,
   smarttab = true,
   cindent = true,
-  wrap = false,
-  backspace = 'indent,eol,start',
   fixeol = false,
   showmatch = true,
   conceallevel = 2,
   concealcursor = '',
-  ignorecase = true,
-  smartcase = true,
-  incsearch = true,
-  hlsearch = true,
   wildignore = '*node_modules/**',
-  completeopt = 'menu,menuone,preview',
-  foldcolumn = '0',
-  foldnestmax = 0,
-  foldlevel = 99,
-  foldlevelstart = 99,
-  swapfile = false,
+  completeopt = 'menu,menuone,noselect',
   writebackup = false,
-  backup = false,
-  undofile = true,
-  viminfo = "'1000",
+  shada = "'1000",
   encoding = 'utf-8',
   fileencoding = 'utf-8',
   clipboard = 'unnamed,unnamedplus',
   errorbells = false,
   timeoutlen = 200,
-  updatetime = 100,
-  splitright = true,
-  splitbelow = true,
-  emoji = false,
-  colorcolumn = '80',
 }
 
 for k, v in pairs(options) do
-  vim.o[k] = v
+  vim.opt[k] = v
 end
 
 -- Глобальные переменные
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 vim.g.speeddating_no_mappings = 1
+vim.g.editorconfig = true
+vim.g.netrw_banner = 0
