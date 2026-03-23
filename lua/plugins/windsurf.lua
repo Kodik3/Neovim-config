754be8e71673c3c9e356049146251c8d8713adbd
Kodik3 <boss.barbashin10@gmail.com>
2026-03-23 09:40:57 +0500

    remove KEYS from windsurf

--- lua/plugins/windsurf.lua
+++ lua/plugins/windsurf.lua
@@ -1,23 +1,14 @@
-local KEYS = {
-  { "<C-g>", function() return vim.fn["codeium#Accept"]() end, mode = "i", expr = true, desc = "Codeium Accept" },
-  { "<C-;>", function() return vim.fn  end, mode = "i", expr = true, desc = "Codeium Next Suggestion" },
-  { "<C-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, mode = "i", expr = true, desc = "Codeium Prev Suggestion" },
-  { "<C-x>", function() return vim.fn["codeium#Clear"]() end, mode = "i", expr = true, desc = "Codeium Clear Suggestion" },
-}
-
 return {
   'Exafunction/windsurf.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'hrsh7th/nvim-cmp',
   },
-
   config = function()
     require("codeium").setup({
       enable_chat = false,
       enable_local_search = false,
       enable_index_service = false,
-      keys = KEYS,
     })
-  end,
+  end
 }
