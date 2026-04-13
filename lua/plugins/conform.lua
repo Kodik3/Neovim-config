483736692b24757513c9bd589202dc58d7182372
Kodik3 <boss.barbashin10@gmail.com>
2026-04-13 07:04:54 +0500

    improve conform config and add ruff support

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,19 +1,33 @@
+local prettier = { 'prettier' }
+
 return {
-  "stevearc/conform.nvim",
+  'stevearc/conform.nvim',
   opts = {
+    notify_on_error = false,
     format_on_save = {
       timeout_ms = 500,
-      lsp_fallback = true,
+    },
+    default_format_opts = {
+      lsp_format = 'fallback',
     },
     formatters_by_ft = {
-      lua = { "stylua" },
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      json = { "prettier" },
-      css = { "prettier" },
-      html = { "prettier" },
-      sh = { "shfmt" },
-      python = { "black" },
+      lua = { 'stylua' },
+      javascript = prettier,
+      typescript = prettier,
+      javascriptreact = prettier,
+      typescriptreact = prettier,
+      json = prettier,
+      css = prettier,
+      html = prettier,
+      sh = { 'shfmt' },
+      rust = { 'rustfmt' },
+      python = function(bufnr)
+        if require('conform').get_formatter_info('ruff_format', bufnr).available then
+          return { 'ruff_fix', 'ruff_format' }
+        else
+          return { 'isort', 'black' }
+        end
+      end,
     },
   },
 }
