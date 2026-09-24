6a10f028b7b241a3a3e99145593394b543f3d690
Kodik3 <boss.barbashin10@gmail.com>
2026-09-24 13:33:50 +0500

    disable luarocks

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,8 +1,14 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+  local out = vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--branch=stable',
+    'https://github.com/folke/lazy.nvim.git',
+    lazypath,
+  })
 
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
@@ -20,4 +26,5 @@ require('lazy').setup({
   concurrency = 50,
   checker = { enabled = true, notify = false },
   ui = { border = 'rounded' },
+  rocks = { enabled = false },
 })
